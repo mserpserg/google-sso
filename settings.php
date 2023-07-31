e081015a058442fb5af89e093cf394a06ac30d04
Sergii <szabolotnyi@mserp.com>
2023-07-31 20:26:51 +0300

    Google SSO - settings.php - Comments update

--- settings.php
+++ settings.php
@@ -1,21 +1,21 @@
 <?php
 
-// Права доступа
+// Access rights
 const GOOGLE_SCOPES = [
-    'https://www.googleapis.com/auth/userinfo.email', // доступ до адреси електронної пошти
-    'https://www.googleapis.com/auth/userinfo.profile' // доступ до інформації профілю
+    'https://www.googleapis.com/auth/userinfo.email', // access to an email address
+    'https://www.googleapis.com/auth/userinfo.profile' // access to profile information
 ];
 
-// Ссылка на аутентификацию
+// Authentication link
 const GOOGLE_AUTH_URI = 'https://accounts.google.com/o/oauth2/auth';
 
-// ссылка на получение токена
+// Link to get token
 const GOOGLE_TOKEN_URI = 'https://accounts.google.com/o/oauth2/token';
 
-// Ссылка на получение информации о пользователе
+// Link to get user information
 const GOOGLE_USER_INFO_URI = 'https://www.googleapis.com/oauth2/v1/userinfo';
 
-// Информация, которая была сгенерирована во время регистрации приложения на google
+// Information that was generated during the registration of the application on google
 
 // Client ID
 const GOOGLE_CLIENT_ID = '123976021744-ppa61c1gtl32cvdf8ai8867orff29ltt.apps.googleusercontent.com';
@@ -23,7 +23,7 @@ const GOOGLE_CLIENT_ID = '123976021744-ppa61c1gtl32cvdf8ai8867orff29ltt.apps.goo
 // Client Secret
 const GOOGLE_CLIENT_SECRET = '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';
 
-// Ссылка из раздела "Authorized redirect URIs"
+// Link from section "Authorized redirect URIs"
 const GOOGLE_REDIRECT_URI = 'http://serg.razumno.com/index.php';
 
 ?>
